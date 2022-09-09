4125c0acded6e028c2c9f92666452c467d413fc3
iknow-git <113151136+iknow-git@users.noreply.github.com>
2022-09-09 08:50:07 +0800

    增加项目
    增加项目

---
+++ TestGit/src/com/fastbj/server/text.java
@@ -0,0 +1,22 @@
+package com.fastbj.server;
+
+public class text {
+    public static void main(String[] args) {
+        System.out.println("git");
+        System.out.println("git hello");
+        System.out.println("git hello.......");
+        System.out.println("在主分支master上进行修改");
+
+        System.out.println("textBranch");
+        System.out.println("textBranch222222");
+
+        System.out.println("第二次在master上修改");
+        System.out.println("在添加delay分支后在master上修改");
+    }
+
+    private void delayCollection(){
+        System.out.println("延迟采集");
+
+        System.out.println("delayCollection");
+    }
+}
